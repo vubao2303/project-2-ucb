2e94340f63365683b7d3d1a401f06d43c54ed7e6
BtramVu <bbbvut@gmail.com>
2021-02-09 21:45:59 -0800

    90% function

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,42 +1,165 @@
 $(document).ready(function () {
-let currentUserId;
+  //Create new party
+  $("#createPartyBtn").on("click", function () {
+    $.post("/api/newparty", {
+      title: $("#eventTitle").val().trim(),
+      theme: $("#eventTheme").val().trim(),
+      date: $("#eventDate").val(),
+      time: $("#eventTime").val(),
+      location: $("#eventLoc").val().trim(),
+    }).then(function () {
+      window.location.reload();
+    });
+  });
 
-	$.get("/api/user_data").then(function (data) {
-		 currentUserId = data.id;
-	});
+  $(document).on("click", ".attend-btn", function (e) {
+    id = e.target.id;
+    $.post("/api/addattendee/" + id).then(() => {
+      window.location.reload();
+    });
+  });
 
+  $(document).on("click", "PLACEHOLDER", function (e) {
+    id = e.target.id;
+    $.get("/api/unattend/" + id).then(() => {
+      window.location.reload();
+    });
+  });
 
+  $.get("/api/hostedparty").then((response) => {
+    hostedHtml(response);
+  });
 
-	// update button in my events component
-$("#createPartyBtn").on("click", event => {
-  event.preventDefault();
-  let btn = event.target;
-  let eventId = $(btn).data("id");
-  let data = {
-    title: $(`#event${eventId}-title`).val().trim(),
-    description: $(`#event${eventId}-description`).val().trim(),
+  $.get("/api/attendingparties").then((response) => {
+    upcomingParties(response);
+  });
+
+  $.get("/api/availableparty").then((response) => {
+    allParties(response);
+  });
+});
+
+function hostedHtml(input) {
+  console.log(input);
+  for (var i = 0; i < input.length; i++) {
+    var html = `<li class="list-group-item" data-id=${input[i].id}>${input[i].title}
+
+	<button
+		class="li-btn"
+		type="button"
+		data-toggle="collapse"
+		data-target="#inputInfo-hosted"
+		aria-expanded="false"
+		aria-controls="inputInfo-hosted">
+		View Info
+	</button>
+</li>
+
+	<div class="collapse" id="inputInfo-hosted">
+		<div class="card card-body">
+			<span>Theme: ${input[i].theme}</span>
+			<span>Date: ${input[i].date}</span>
+			<span>Time: ${input[i].time}</span>
+			<span>Location: ${input[i].location}</span>
+			<hr />
+			<span><button id=${input[i].id} class="li-btn-delete" data-id=${input[i].id}>Delete</button></span>
+		</div>
+	</div>
+		`;
+
+    $(".userEvents").append(html);
+
+    $(document).on("click", ".li-btn-delete", (e) => {
+      console.log(e.target.id);
+      console.log($(this));
+      var id = e.target.id;
+      console.log(id);
+      $.ajax({
+        url: "/api/hostedparty/" + id,
+        method: "DELETE",
+      }).then(() => {
+        console.log("Deleted");
+        window.location.reload();
+      });
+    });
   }
-  updateEvent(eventId, data).then(() => {
-    window.location.replace("./main")
-  })
-})
-});
-// trying tringtr atiorfdpfakjdhfka
-$("#createPartyBtn").on(`submit`, event => {
-	event.preventDefault();
+}
 
-	var newparty = {
-		name: $("#eventTitle").val().trim()
-	};
+function allParties(input) {
+  for (var i = 0; i < input.length; i++) {
+    var html = `<li class="list-group-item">${input[i].title}
+  <button
+    class="li-btn"
+    data-id=""
+    data-toggle="collapse"
+    data-target="#eventInfo-all"
+    >View Party</button>
+  </li>
 
-	$.ajax("/api/newparty", {
-		type: `POST`,
-		data: newparty
-	}).then(() => {
-		location.reload();
-	});
+  <div class="collapse" id="eventInfo-all">
+    <div class="card card-body">
+
+      <p class="host-name">Host: $input[i].name}</p> 
 
 
+      <p>Theme:${input[i].theme}</p>
+      <p>Location:${input[i].location}</p>
+      <p>Date:${input[i].date}</p>
+      <p>Time:${input[i].time}</p>
+      <hr />
+      <button id=${input[i].id} class="attend-btn li-btn" data-id=${input[i].id}>Attend Party</button>
+    </div>
+  </div>`;
 
+    $(".allEvents-list").append(html);
+  }
+}
 
-});
+function upcomingParties(input) {
+  // console.log(input);
+  for (var i = 0; i < input.length; i++) {
+    var html = `<li class="list-group-item" data-id=${input[i].id}>${input[i].title}
+
+	<button
+		class="li-btn"
+		type="button"
+		data-toggle="collapse"
+		data-target="#inputInfo-hosted"
+		aria-expanded="false"
+		aria-controls="inputInfo-hosted">
+		View Info
+	</button>
+</li>
+
+	<div class="collapse" id="inputInfo-hosted">
+		<div class="card card-body">
+
+      <p class="host-name">Host: ${input[i].name}</p>
+
+			<span>Theme: ${input[i].theme}</span>
+			<span>Date: ${input[i].date}</span>
+			<span>Time: ${input[i].time}</span>
+			<span>Location: ${input[i].location}</span>
+			<hr />
+			<span><button id=${input[i].id} class="li-btn-delete" data-id=${input[i].id}>Delete</button></span>
+		</div>
+	</div>
+		`;
+
+    $(".attendEvents").append(html);
+
+    $(document).on("click", ".li-btn-delete", (e) => {
+      console.log(e.target.id);
+      console.log($(this));
+      var id = e.target.id;
+      console.log(id);
+      $.ajax({
+        url: "/api/attendingparties/" + id,
+        method: "DELETE",
+      }).then(() => {
+        console.log("Deleted");
+        window.location.reload();
+      });
+    });
+  }
+}
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,106 +1,172 @@
 // Requiring our models and passport as we've configured it
 var db = require("../models");
 var passport = require("../config/passport");
-// const { regexp } = require("sequelize/types/lib/operators");
+// Sequelize Operator
+var { Op } = require("sequelize");
 
 module.exports = function (app) {
-	// Using the passport.authenticate middleware with our local strategy.
-	// If the user has valid login credentials, send them to the members page.
-	// Otherwise the user will be sent an error
-	app.post("/api/signin", passport.authenticate("local"), function (req, res) {
-		res.json(req.user);
-	});
+  // Using the passport.authenticate middleware with our local strategy.
+  // If the user has valid login credentials, send them to the members page.
+  // Otherwise the user will be sent an error
+  app.post("/api/signin", passport.authenticate("local"), function (req, res) {
+    res.json(req.user);
+  });
 
-	// Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-	// how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-	// otherwise send back an error
-	app.post("/api/newuser", function (req, res) {
-		db.User.create({
-			name: req.body.name,
-			email: req.body.email,
-			password: req.body.password
-		})
-			.then(function () {
-				res.redirect(307, "/api/signin");
-			})
-			.catch(function (err) {
-				res.status(401).json(err);
-			});
-	});
-
-	// Route for getting some data about our user to be used client side
-	app.get("/api/user_data", function (req, res) {
-		if (!req.user) {
-			// The user is not logged in, send back an empty object
-			res.json({});
-		} else {
-			// Otherwise send back the user's email and id
-			// Sending back a password, even a hashed password, isn't a good idea
-			res.json({
-				email: req.user.email,
-				id: req.user.id,
-				name: req.user.name
-			});
-		}
-	});
-
-	app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
-	// Create route to attend a new party ; PUT request
-	app.put("/api/attend/:id"), function (req, res) {
-		db.User.update({
-			where : {
-				id: req.params.id
-			}
-		})
-	};
 
-	// Create a new party
-	app.post("/api/newparty", function (req, res) {
-		console.log(req.body);
-		db.Party.create({
-			title: req.body.title,
-			theme: req.body.theme,
-			date: req.body.date,
-			time: req.body.time,
-			location: req.body.location,
-			hostId: req.body.hostId
-		})
-			.then(function(){
-				res.send(200);
-			})
-			// .catch(function(err){
-			// 	res.status(401).json(err);
-			// })
-	});
-
-	// Create find parties route
-	app.get("/api/findparties"), function (req, res) {
-		db.Party.findAll({}).then(function(dbParty){
-			res.json(dbParty);
-		})
-	};
-
-	// Route to present parties on userdashboard
-	app.get("/api/userdashboard"), function(req,res){
-		db.Party.findAll({
-
-		})
-	}
-//  bbbbbtrying
-app.post("/api/newparty", (req, res) => {
-  burger.create(["eventTitle"], [req.body.name], result => {
-    res.json({ id: result.insertId });
+  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
+  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
+  // otherwise send back an error
+  app.post("/api/newuser", function (req, res) {
+    db.User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    })
+      .then(function () {
+        res.redirect(307, "/api/signin");
+      })
+      .catch(function (err) {
+        res.status(401).json(err);
+      });
   });
-});
 
+  // Route for getting some data about our user to be used client side
+  app.get("/api/user_data", function (req, res) {
+    if (!req.user) {
+      // The user is not logged in, send back an empty object
+      res.json({});
+    } else {
+      // Otherwise send back the user's email and id
+      // Sending back a password, even a hashed password, isn't a good idea
+      res.json({
+        email: req.user.email,
+        id: req.user.id,
+        name: req.user.name,
+      });
+    }
+  });
+
+  app.get("/api/hostedparty", (req, res) => {
+    db.Party.findAll({
+      where: {
+        hostId: req.user.id,
+      },
+    }).then((data) => {
+      res.json(data);
+    });
+  });
+
+  app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+  });
+
+  // Create a new party
+  app.post("/api/newparty", function (req, res) {
+    console.log(req.body);
+    db.Party.create({
+      title: req.body.title,
+      theme: req.body.theme,
+      date: req.body.date,
+      time: req.body.time,
+      location: req.body.location,
+      hostId: req.user.id,
+    }).then(function () {
+      res.send(200);
+    });
+    // .catch(function(err){
+    // 	res.status(401).json(err);
+    // })
+  });
+
+  // route used to get all events
+  app.get("/api/availableparty", (req, res) => {
+    // search Event table for all events
+    console.log("made it to events");
+    db.Party.findAll({
+      // join User since it contains the host's name
+      where: {
+        hostId: {
+          [Op.not]: req.user.id,
+        },
+      },
+      include: [
+        {
+          model: db.User,
+          as: "host",
+          attributes: ["name"],
+        },
+      ],
+    })
+      .then((events) => {
+        res.json(events);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(false);
+      });
+  });
+  // Attend party route
+  app.get("/api/attendingparties", function (req, res) {
+    console.log("Made it to attend party");
+    db.UserParty.findAll({
+      where: {
+        UserId: req.user.id,
+      },
+      include: {
+        model: db.Party,
+        include: {
+          model: db.User,
+          as: "host",
+          attributes: ["name"],
+        },
+      },
+    }).then(function (dbUserParty) {
+      console.log("User Party xxxxxxxxx" + dbUserParty);
+      let data = [];
+      dbUserParty.forEach((user) => {
+        data.push(user.Party);
+      });
+      res.json(data);
+    });
+  });
+
+  app.post("/api/addattendee/:id", function (req, res) {
+    db.UserParty.create({
+      PartyId: req.params.id,
+      UserId: req.user.id,
+    }).then(function (data) {
+      console.log(data);
+      res.json(data);
+    });
+  });
+
+  app.delete("/api/hostedparty/:id", (req, res) => {
+    db.Party.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+      .then(() => {
+        res.send(true);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(false);
+      });
+  });
+
+  app.delete("/api/unattend/:id", function (req, res) {
+    db.UserParty.destroy({
+      where: {
+        [Op.and]: [{ PartyId: req.param.id }, { UserId: req.user.id }],
+      },
+    }).then(function () {
+      res.send(200);
+    });
+  });
 };
-
-
-  // Route for logging user out
-//   app.get("/logout", function(req, res) {
-//     req.logout();
-//     res.redirect("/");
-//   });
